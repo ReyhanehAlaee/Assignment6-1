4edc0ec527521adb4b68e5becd044184842f6030
ReyhanehAlaee <148444766+ReyhanehAlaee@users.noreply.github.com>
2023-11-11 00:10:53 +0330

    Add files via upload

---
+++ Asignment6-1.py
@@ -0,0 +1,82 @@
+def show():
+    for row in game_board:
+        for cell in row:
+            print(cell, end=" ")
+        print()
+
+def check():
+    if game_board[0][0] == "O" and game_board[0][1] == "O" and game_board[0][2] == "O":
+        print("Player1 win!")
+    if game_board[1][0] == "O" and game_board[1][1] == "O" and game_board[1][2] == "O":
+        print("Player1 win!")
+    if game_board[2][0] == "O" and game_board[2][1] == "O" and game_board[2][2] == "O":
+        print("Player1 win!")
+    if game_board[0][0] == "O" and game_board[1][0] == "O" and game_board[2][0] == "O":
+        print("Player1 win!")
+    if game_board[0][1] == "O" and game_board[1][1] == "O" and game_board[2][1] == "O":
+        print("Player1 win!")
+    if game_board[0][2] == "O" and game_board[1][2] == "O" and game_board[2][2] == "O":
+        print("Player1 win!")
+    if game_board[0][0] == "O" and game_board[1][1] == "O" and game_board[2][2] == "O":
+        print("Player1 win!")
+    if game_board[0][2] == "O" and game_board[1][1] == "O" and game_board[2][0] == "O":
+        print("Player1 win!")
+    
+    if game_board[0][0] == "X" and game_board[0][1] == "X" and game_board[0][2] == "X":
+        print("Player2 win!")
+    if game_board[1][0] == "X" and game_board[1][1] == "X" and game_board[1][2] == "X":
+        print("Player2 win!")
+    if game_board[2][0] == "X" and game_board[2][1] == "X" and game_board[2][2] == "X":
+        print("Player2 win!")
+    if game_board[0][0] == "X" and game_board[1][0] == "X" and game_board[2][0] == "X":
+        print("Player2 win!")
+    if game_board[0][1] == "X" and game_board[1][1] == "X" and game_board[2][1] == "X":
+        print("Player2 win!")
+    if game_board[0][2] == "X" and game_board[1][2] == "X" and game_board[2][2] == "X":
+        print("Player2 win!")
+    if game_board[0][0] == "X" and game_board[1][1] == "X" and game_board[2][2] == "X":
+        print("Player2 win!")
+    if game_board[0][2] == "X" and game_board[1][1] == "X" and game_board[2][0] == "X":
+        print("Player2 win!")
+
+game_board = [["-","-","-"],
+              ["-","-","-"],
+              ["-","-","-"]]
+
+show()
+while True:
+    print("Player1") # => O
+    while True:
+            row = int(input("Enter row: "))
+            col = int(input("Enter col: "))
+            if 0<= row <=2 and 0<= col <=2:
+                if game_board[row][col] == "-":
+                    game_board[row][col] = "O" 
+                    show()
+                    check()
+                    break 
+                else:
+                    print("Yek khone dige vard kon!")
+            else:
+                print("Bein 0 va 2 vared kon!")
+    if game_board[0][0] != "-" and game_board[0][1] != "-" and game_board[0][2] != "-" and game_board[1][0] != "-" and game_board[1][1] != "-" and game_board[1][2] != "-" and game_board[2][0] != "-" and game_board[2][1] != "-" and game_board[2][2] != "-" and check() != True:
+                    print("equal")
+                    break
+    
+    print("Player2") # => X
+    while True:
+            row = int(input("Enter row: "))
+            col = int(input("Enter col: "))
+            if 0<= row <=2 and 0<= col <=2:
+                if game_board[row][col] == "-":
+                    game_board[row][col] = "X"
+                    show()
+                    check()
+                    break
+                else:
+                    print("Yek khone dige vard kon!")
+            else:
+                print("Bein 0 va 2 vared kon!")
+    if game_board[0][0] != "-" and game_board[0][1] != "-" and game_board[0][2] != "-" and game_board[1][0] != "-" and game_board[1][1] != "-" and game_board[1][2] != "-" and game_board[2][0] != "-" and game_board[2][1] != "-" and game_board[2][2] != "-" and check() != True:
+         print("equal")
+         break
